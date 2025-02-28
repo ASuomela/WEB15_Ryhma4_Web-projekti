fe29fd76ac65ada2d1e83c87564fbb7c87cbe54f
ilskii <ilari.tuominen@outlook.com>
2025-02-28 10:53:08 +0200

    Laskeutumissivun lisääminen

---
+++ javascript-index.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    setTimeout(function() {
+        document.querySelector('.landing-text').classList.add('appear');
+    }, 3000); // Pieni viive ilmestymisen pehmentämiseksi
+});
