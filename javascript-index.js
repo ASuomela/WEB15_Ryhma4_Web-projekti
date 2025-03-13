829862578662bd76b20a2f6521c1b7022a228ef3
ilskii <ilari.tuominen@outlook.com>
2025-03-13 15:15:55 +0200

    Landing page -päivitykset

--- javascript-index.js
+++ javascript-index.js
@@ -3,3 +3,14 @@ window.addEventListener('load', function() {
         document.querySelector('.landing-text').classList.add('appear');
     }, 3000); // Pieni viive ilmestymisen pehmentämiseksi
 });
+
+document.getElementById('toggle-music-btn').addEventListener('click', function() {
+    const music = document.getElementById('background-music');
+    if (music.muted) {
+        music.muted = false; // Musiikin unmute
+        this.textContent = 'Mykistä musiikki'; // Päivitä napin teksti
+    } else {
+        music.muted = true; // Mute the audio
+        this.textContent = 'Poista mykistys'; // Päivitä napin teksti
+    }
+});
